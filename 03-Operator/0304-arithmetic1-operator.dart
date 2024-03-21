b849f76b22e08bd5dfddc3fcac748dc8a417cffb
Sanjna134 <sanjnasolanki134@gmail.com>
2024-03-21 12:48:35 +0530

    add

---
+++ 03-Operator/0304-arithmetic1-operator.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 99;
+  int b = 12;
+  int c;
+  print("add value of c is: ${a + b}");
+  print("sub value of c is: ${a - b}");
+  print("mul value of c is: ${a * b}");
+  print("div value of c is: ${a / b}");
+  print("mod value of c is: ${a % b}");
+}
---
+++ 03-Operator/0305-assignment-operator.dart/0305-4.dart
@@ -0,0 +1,5 @@
+void main() {
+  int a = 25;
+  int b = 8;
+  print(a %= b);
+}
