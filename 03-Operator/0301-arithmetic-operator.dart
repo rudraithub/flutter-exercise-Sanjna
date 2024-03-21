e94a93eaf0a043c736e17863014e6fe2afeb3e5f
Sanjna134 <sanjnasolanki134@gmail.com>
2024-03-21 12:37:40 +0530

    add

---
+++ 03-Operator/0301-arithmetic-operator.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 56;
+  int b = 80;
+  print("addition is: ${a + b}");
+  print("substraction is: ${a - b}");
+  print("multiplication is: ${a * b}");
+  print("division is: ${a / b}");
+  print("module is: ${a % b}");
+}
