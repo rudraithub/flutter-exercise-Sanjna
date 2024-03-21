ddb361a7a66677ff596a805cbf547ca0eb43c4db
Sanjna134 <sanjnasolanki134@gmail.com>
2024-03-21 14:40:02 +0530

    add

---
+++ 10-map/1001-map.dart/1.1-print valu-map.dart
@@ -0,0 +1,10 @@
+void main() {
+  Map<String, dynamic> s = {
+    'name': 'sanjna',
+    'mobileno': 9658452125,
+    'address': 'bhavnagar',
+    'course': ['Android', 'Ios', 'Java', 'Swift']
+  };
+
+  print(s);
+}
