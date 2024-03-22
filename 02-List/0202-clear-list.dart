3c180a8d5b951e77fc0b59f4e5cce1da733614c9
Sanjna134 <sanjnasolanki134@gmail.com>
2024-03-22 10:49:12 +0530

    update

--- 02-List/0202-clear-list.dart
+++ 02-List/0202-clear-list.dart
@@ -1,6 +1,6 @@
 void main() {
-  var list = [10, 20, 30];
-  print('List before clear element:$list');
-  list.clear();
-  print('List after clear element:$list');
+  List n = [10, 20, 30];
+  print('List before clear element:$n');
+  n.clear();
+  print('List after clear element:$n');
 }
