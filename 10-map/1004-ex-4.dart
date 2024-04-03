5c70c80763620e7d637a6d16ada23a970c59913b
Sanjna134 <sanjnasolanki134@gmail.com>
2024-04-03 10:32:54 +0530

    add

---
+++ 10-map/1004-ex-4.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map<String, dynamic> s = {
+    'AccountNumber': 255551515555555,
+    'AccountHolder': 'krish',
+    'BankName': 'SBI',
+    'Branch': 'ADB',
+    'IfscCode': 'SBIN0010940',
+    'Balance': 500000
+  };
+
+  print(s);
+}
---
+++ 10-map/1005-ex-5.dart
@@ -0,0 +1,12 @@
+void main() {
+  Map<String, dynamic> s = {
+    'President': 'Droupadi murmu',
+    'Primeminister': 'Narendra Modi',
+    'Totalseats': 30,
+    'oppositionparties': 'Congress',
+    'GovernmentType': 'Democratic',
+    'city': 'Bhavnagar'
+  };
+
+  print(s);
+}
