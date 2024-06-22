406ba8becfed6fac466981f68aa855f7bf1bbce8
Sanjna134 <sanjnasolanki134@gmail.com>
2024-06-22 23:08:36 +0530

    add

---
+++ 22-textformfield/textformfield_2201/android/app/src/main/kotlin/com/example/textformfield_2201/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.textformfield_2201
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 22-textformfield/textformfield_2202/android/app/src/main/kotlin/com/example/textformfield_2202/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.textformfield_2202
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
